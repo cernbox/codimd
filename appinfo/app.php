211f33f30f132314bd0126e2a887b38681eceb7f
Giuseppe Lo Presti <giuseppe.lopresti@cern.ch>
2021-10-08 13:48:26 +0200

    Add the "new." subdomain as well for compatibility with OCIS

--- appinfo/app.php
+++ appinfo/app.php
@@ -4,7 +4,7 @@ namespace OCA\CodiMD\AppInfo;
 
 $app = new Application();
 
-$domains = \OC::$server->getConfig()->getSystemValue("cbox.wopi.officeonline", ['https://login.cern.ch', 'https://oos.web.cern.ch', 'https://msoo-lb.cern.ch', 'https://wopi.cernbox.cern.ch:8443', 'https://cbox-wopidev-01.cern.ch:8443', 'https://msoo-lb-dev.cern.ch']);
+$domains = \OC::$server->getConfig()->getSystemValue("cbox.wopi.officeonline", ['https://login.cern.ch', 'https://new.cernbox.cern.ch', 'https://oos.web.cern.ch', 'https://msoo-lb.cern.ch', 'https://wopi.cernbox.cern.ch:8443', 'https://cbox-wopidev-01.cern.ch:8443', 'https://msoo-lb-dev.cern.ch']);
 $policy = new \OCP\AppFramework\Http\EmptyContentSecurityPolicy();
 foreach($domains as $domain) {
 	$policy->addAllowedScriptDomain($domain);
